e067fef0afe1cc3e041ec7a034c985ce5fd23d82
Cauê Almeida <nncl@live.com>
2017-05-15 20:42:30 -0300

    Adicionar arquivos de projeto.

---
+++ API.AplicativoMOB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API.AplicativoMOB.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ API.AplicativoMOB/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API.AplicativoMOB.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
